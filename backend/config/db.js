74d3f020ea77a715e1950ed9a8e112223a339b87
jeeshian <jeeshian2001@gmail.com>
2024-09-23 20:27:58 +0800

    connected to mongoDB

---
+++ backend/config/db.js
@@ -0,0 +1,13 @@
+// connecting to the database
+import mongoose from "mongoose";
+import { ENV_VARS } from "./envVars.js";
+
+export const connectDB = async () => {
+  try {
+    const conn = await mongoose.connect(ENV_VARS.MONGO_URI);
+    console.log(`MongoDB connected: ${conn.connection.host}`);
+  } catch (error) {
+    console.log(`MongoDB connection error: ${error}`);
+    process.exit(1);
+  }
+};
--- backend/server.js
+++ backend/server.js
@@ -1,17 +1,14 @@
-import express from 'express';
-import authRoutes from './routes/auth.route.js';
-import { ENV_VARS } from './config/envVars.js';
-
+import express from "express";
+import authRoutes from "./routes/auth.route.js";
+import { ENV_VARS } from "./config/envVars.js";
+import { connectDB } from "./config/db.js";
 
 const app = express();
 
+app.use("/api/v1/auth", authRoutes);
 
-
-app.use('/api/v1/auth', authRoutes)
-console.log(`MONGO_URI: ${ENV_VARS.MONGO_URI}`);
-
-const port = ENV_VARS.PORT
+const port = ENV_VARS.PORT;
 app.listen(port, () => {
-    console.log(`server running on http://localhost:${port}`);
+  console.log(`server running on http://localhost:${port}`);
+  connectDB();
 });
-
