79239dad1b672e979c19af96a8944eccd1f1e2d9
jeeshian <jeeshian2001@gmail.com>
2024-09-24 23:37:31 +0800

    done search endpoints

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,10 @@
 import express from "express";
+
 import authRoutes from "./routes/auth.route.js";
 import movieRoutes from "./routes/movie.route.js";
 import tvRoutes from "./routes/tv.route.js";
+import searchRoutes from "./routes/search.route.js";
+
 import { ENV_VARS } from "./config/envVars.js";
 import { connectDB } from "./config/db.js";
 import cookieParser from "cookie-parser";
@@ -15,6 +18,7 @@ app.use(cookieParser());
 app.use("/api/v1/auth", authRoutes);
 app.use("/api/v1/movie", protectRoute, movieRoutes); // we don't want unauthenticated user to access the movie, therefore we include a protectRoute middleware to prevent this
 app.use("/api/v1/tv", protectRoute, tvRoutes);
+app.use("/api/v1/search", protectRoute, searchRoutes);
 
 const port = ENV_VARS.PORT;
 app.listen(port, () => {
