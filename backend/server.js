4beaa8d4dba00ee6d5fa4e01473073189727a5eb
jeeshian <jeeshian2001@gmail.com>
2024-09-24 22:13:22 +0800

    done middleware to protectRoute

--- backend/server.js
+++ backend/server.js
@@ -4,14 +4,17 @@ import movieRoutes from "./routes/movie.route.js";
 import tvRoutes from "./routes/tv.route.js";
 import { ENV_VARS } from "./config/envVars.js";
 import { connectDB } from "./config/db.js";
+import cookieParser from "cookie-parser";
+import { protectRoute } from "./middleware/protectRoute.js";
 
 const app = express();
 
 app.use(express.json()); // allow the request body to be parsed as JSON
+app.use(cookieParser());
 
 app.use("/api/v1/auth", authRoutes);
-app.use("/api/v1/movie", movieRoutes);
-app.use("/api/v1/tv", tvRoutes);
+app.use("/api/v1/movie", protectRoute, movieRoutes); // we don't want unauthenticated user to access the movie, therefore we include a protectRoute middleware to prevent this
+app.use("/api/v1/tv", protectRoute, tvRoutes);
 
 const port = ENV_VARS.PORT;
 app.listen(port, () => {
