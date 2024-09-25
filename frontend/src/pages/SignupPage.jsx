7578bb8c908e49b238cb83d17f7366263f7749f7
jeeshian <jeeshian2001@gmail.com>
2024-09-25 12:41:08 +0800

    done signup page

--- frontend/src/pages/SignupPage.jsx
+++ frontend/src/pages/SignupPage.jsx
@@ -1,7 +1,103 @@
-import React from "react";
+import { useState } from "react";
+import { Link } from "react-router-dom";
 
 const SignupPage = () => {
-  return <div>SignupPage</div>;
+  const [email, setEmail] = useState("");
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleSignup = async (e) => {
+    e.preventDefault(); // prevent page to refresh after submitting
+    console.log(email, username, password);
+  };
+
+  return (
+    <div className="h-screen w-full hero-bg">
+      <header className="max-w-6xl mx-auto flex items-center justify-between p-4">
+        <Link to={"/"}>
+          <img src="/netflix-logo.png" alt="logo" className="w-52" />
+        </Link>
+      </header>
+
+      <div className="flex justify-center items-center mt-20 mx-3">
+        <div className="w-full max-w-md p-8 space-y-6 bg-black/60 rounded-lg shadow-md">
+          <h1 className="text-white text-center text-2xl font-bold mb-4">
+            Sign Up
+          </h1>
+
+          <form className="space-y-4" onSubmit={handleSignup}>
+            <div>
+              <label
+                htmlFor="email"
+                className="text-sm font-medium text-gray-300 block"
+              >
+                Email
+              </label>
+              <input
+                type="email"
+                id="email"
+                required
+                className="w-full px-3 py-2 border mt-1 border-gray-700 bg-transparent
+                rounded-md text-white focus:outline-none focus:ring"
+                placeholder="you@example.com"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+              />
+            </div>
+
+            <div>
+              <label
+                htmlFor="username"
+                className="text-sm font-medium text-gray-300 block"
+              >
+                Username
+              </label>
+              <input
+                type="text"
+                id="Username"
+                required
+                className="w-full px-3 py-2 border mt-1 border-gray-700 bg-transparent
+                rounded-md text-white focus:outline-none focus:ring"
+                placeholder="username"
+                value={username}
+                onChange={(e) => setUsername(e.target.value)}
+              />
+            </div>
+
+            <div>
+              <label
+                htmlFor="password"
+                className="text-sm font-medium text-gray-300 block"
+              >
+                Password
+              </label>
+              <input
+                type="password"
+                id="password"
+                required
+                className="w-full px-3 py-2 border mt-1 border-gray-700 bg-transparent
+                rounded-md text-white focus:outline-none focus:ring"
+                placeholder="******"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+              />
+            </div>
+
+            <button className="w-full py-2 bg-red-600 text-white font-semibold rounded-md hover:bg-red-700">
+              Sign Up
+            </button>
+          </form>
+
+          <div className="text-gray-400 text-center">
+            Already a member?{" "}
+            <Link to={"/login"} className="text-red-500 hover:underline">
+              Login
+            </Link>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default SignupPage;
